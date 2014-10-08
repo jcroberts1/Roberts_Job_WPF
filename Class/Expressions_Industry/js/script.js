ac5e4e54ce2c3702687dd9fd7b096c284666edb0
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-07 22:49:38 -0500

    Class Folder
    This is my class folder

---
+++ Class/Expressions_Industry/js/script.js
@@ -0,0 +1,7 @@
+// JavaScript
+
+var age = 34; //Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Brandon";
---
+++ Class/Expressions_Personal/js/script.js
@@ -0,0 +1,14 @@
+//JavaScript
+// Name: Job Roberts
+// Date: 06 Oct 2014
+//Assignment:Expressions
+// How much to feed your dog or puppy.
+
+var num1=prompt("enter dogs weight");
+var num2=prompt("enter calories per serving");
+var cups=num1 * num2;
+
+var result = "You should feed your pet" + cups + "cups";
+alert(num1*num2);
+alert(result);
+

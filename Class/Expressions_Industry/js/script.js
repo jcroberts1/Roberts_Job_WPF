9eefe81f6e9b29f32c5d3cb58f8f93f62f64745d
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-09 18:15:03 -0500

    Updated Title

--- Class/Expressions_Industry/js/script.js
+++ Class/Expressions_Industry/js/script.js
@@ -4,13 +4,13 @@
  //WPF 1410 Section 01
  // Date: 06 Oct 2014
  //Assignment:Expressions
- // Web design tools.
+ // Create Website.
  */
 //
 
 
 //New Array added
-var tools =["Laptop","Software", "Hosting", "domainName"];//(0,1,2,3)
+var webTools =["Laptop","Software", "Hosting", "domainName"];//(0,1,2,3)
 
 
 var tools =[0];

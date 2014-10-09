4e033d64396f7c3e97fafac911a74df41c599024
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-08 21:25:29 -0500

    Updated comments
    Updated comments to the script file

--- Class/Expressions_Personal/js/script.js
+++ Class/Expressions_Personal/js/script.js
@@ -8,24 +8,32 @@
 */
 // Ask the user his or her name
 var name = prompt("Enter Your name");
-// greeting to the user
+
+
+// Welcome greeting to the user
 alert("Welcome  "+name+"! Let's find out how long to pay of a debt");
+
+
 // First prompt for the user
 var balanceDue = prompt("Enter you current Balance on bill");
-console.log(balanceDue);
+console.log(balanceDue);//console.log response
+
 
 //lets gather monthly payments
 var currentPayments = prompt("What is your monthly payment");
-console.log(currentPayments);
+console.log(currentPayments);//console.log response
+
+
 
 //Calculate the payoff
 var total = balanceDue / currentPayments ;
-console.log(total);//console.log
+console.log(total);//console.log response
+
 
 //Calculate pay off months
 console.log("You will pay off " +balanceDue+" in "+total+" Months.");
 alert("You will pay off " +balanceDue+" in "+total+" Months.");
-
+// console.log should match alert box
 
 
 

9d6adce85564999fcdef19feeccaea35771ac7ab
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-09 18:11:55 -0500

    New Html created

--- Class/Expressions_Industry/js/script.js
+++ Class/Expressions_Industry/js/script.js
@@ -4,10 +4,14 @@
  //WPF 1410 Section 01
  // Date: 06 Oct 2014
  //Assignment:Expressions
- // .Web design tools.
+ // Web design tools.
  */
 //
 
 
 //New Array added
-var tools =["Laptop","Software", "Hosting", "domainName"];
+var tools =["Laptop","Software", "Hosting", "domainName"];//(0,1,2,3)
+
+
+var tools =[0];
+console.log(tools);

db9d23d1bc2c07183546e561a4f9f683bf72e81d
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-09 18:59:38 -0500

    Updated js code

--- Class/Expressions_Industry/js/script.js
+++ Class/Expressions_Industry/js/script.js
@@ -8,18 +8,8 @@
  */
 //
 
-// Ask the user his or her name
-var name = prompt("Enter Your name");
-
-// welcome message
-var welcome = ("What you need to create a website");
-
-// Welcome greeting to the user
-alert("Hi  "+name+"! Let's create a website");
 
 
 
-//New Array added
-var webTools =["Laptop", "Software", "Hosting", "domainName" , "creativity" ,"Imagination"];//0,1,2,3,4,5
 
 

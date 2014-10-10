47fa46a4238aa484d9308b7b33fb7d3262341947
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-09 19:44:33 -0500

    Set up calculation for tired weight of car

--- Class/Expressions_Industry/js/script.js
+++ Class/Expressions_Industry/js/script.js
@@ -4,11 +4,45 @@
  //WPF 1410 Section 01
  // Date: 06 Oct 2014
  //Assignment:Expressions
- // Create Website.
+ // Tired Weight of your car.
  */
 //
 
+// Ask the user his or her name
+var name = prompt("Enter Your name");
 
+// Welcome greeting to the user
+alert("Welcome  "+name+"! Let's find out the weight of you car");
+
+alert("You will need thin cardboard,ruler,& tire gage in PSI(pound per square inch) ");
+//This is the materials you will need to do this
+
+alert("Get a footprint of tire on cardboard to measure length and width");
+//instructions to get length and width
+// First prompt for the user
+var length = prompt("Enter length of tire");
+console.log(length);//console.log response
+
+//Prompt
+var width = prompt("Enter width of tire");
+console.log(width);//console.log response
+
+
+
+
+
+
+
+
+//Calculate the payoff
+var total = balanceDue / currentPayments ;
+console.log(total);//console.log response
+
+
+//Calculate pay off months
+console.log("You will pay off " +balanceDue+" in "+total+" Months.");
+alert("You will pay off " +balanceDue+" in "+total+" Months.");
+// console.log should match alert box
 
 
 

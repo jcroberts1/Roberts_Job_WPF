7758ceb31cc3c7f2b2b67515cc5d19237978e3eb
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-09 21:28:48 -0500

    Updated title

--- Class/Expressions_Wacky/js/script.js
+++ Class/Expressions_Wacky/js/script.js
@@ -2,8 +2,31 @@
  //JavaScript
  // Name: Job Roberts
  //WPF 1410 Section 01
- // Date: 09 Oct 2014
+ // Date: 06 Oct 2014
  //Assignment:Expressions
- // .
+ // Determine gas mileage.
  */
-//);
+// Ask the user his or her name
+var name = prompt("Enter Your name");
+
+// Welcome greeting to the user
+alert("Welcome  "+name+"! Let's find out how long to pay of a debt");
+
+// First prompt for the user
+var balanceDue = prompt("Enter you current Balance on bill");
+console.log(balanceDue);//console.log response
+
+//Prompt to gather monthly payments
+var currentPayments = prompt("What is your monthly payment");
+console.log(currentPayments);//console.log response
+
+
+//Calculate the payoff
+var total = balanceDue / currentPayments ;
+console.log(total);//console.log response
+
+
+//Calculate pay off months
+console.log("You will pay off " +balanceDue+" in "+total+" Months.");
+alert("You will pay off " +balanceDue+" in "+total+" Months.");
+// console.log should match alert box

f52520116fd9059d47474d506e30c966a9f81fa2
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-12 13:27:43 -0500

    New folder created
    Exercise  folder for practice

---
+++ Class/Expressions_Wacky/images/js.js
@@ -0,0 +1,8 @@
+var oldEnough = true;
+
+//if the child is old enough , print to the console "you can ride!"
+
+if(oldEnough){
+    //code performed if condition true
+    console.log("you can ride the coster!");
+}

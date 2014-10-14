fc82666255101fe401c113596c298fbb5017425d
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-14 07:07:42 -0500

    Conditionals_worksheet

---
+++ Class/Conditionals_worksheet/js/js.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jobroberts on 10/13/14.
+ */
+
+
+//Hot Enough?<br> //Is it hot enough to go to the beach?<br>
+
+var temp = 80;
+//if the temperature is less than 75
+
+if(temp < 75){
+    console.log(“We will go to the beach!”);
+    <br> }else{
+        <br>      //if it’s greater or equal to 75<br>
+    console.log(“We will go to the movies.”);<br> }
+}
---
+++ Class/Conditionals_worksheet/js/script.js
@@ -0,0 +1,32 @@
+    /*
+     //JavaScript
+     // Name: Job Roberts
+     //WPF 1410 Section 01
+     // Date: 12 Oct 2014
+     //Assignment:Conditionals_worksheet
+     */
+
+var miles = 200;
+
+var mpg = prompt("Enter Miles per gallon");
+    console.log(mpg);
+
+var gasReading = prompt("Gauge reading of the gas tank");
+    console.log(gasReading);
+
+var capacity = prompt("Enter gas tank capacity");
+    console.log(capacity);
+
+var total = miles / gasReading;
+
+var  gasStation = 200;
+
+  if(gasStation){
+      console.log("Yes,you can make it with out stopping for gas!")
+
+  }else{
+      console.log("You only have "+gasReading+" percent left, better stop for gas.");
+
+    }
+
+

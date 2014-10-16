18db4e744b3bb87d283a2ac9a029d9cad69d7c46
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-16 17:20:05 -0500

    Initial commit

--- Class/Conditionals_Industry/js/script.js
+++ Class/Conditionals_Industry/js/script.js
@@ -4,15 +4,16 @@
  //WPF 1410 Section 01
  // Date: 14 Oct 2014
  //Assignment:Conditionals
- // Conditionals_Industry.
+ // Conditionals_Photo printing.
  */
 //
 
 var minRes = 300;
 var photo= 500;
 
-if (minRes < photo) {
+if (minRes > photo) {
     console.log("You can print the photo!");
 } else {
     console.log("You can not print the photo!");
 }
+

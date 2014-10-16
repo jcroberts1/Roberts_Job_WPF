4198f80f86c3e1b2dc0b68db10f950d1ffb72691
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-16 17:56:20 -0500

    if and else statements entered

--- Class/Conditionals_Industry/js/script.js
+++ Class/Conditionals_Industry/js/script.js
@@ -9,11 +9,38 @@
 //
 
 var minRes = 300;
-var photo= 500;
+var photo = 500;
+var fileSize =  900;
 
-if (minRes > photo) {
-    console.log("You can print the photo!");
+// condition if your photo is to small resolution you can not print , how ever if your resolution is high enough you can print!
+
+if (minRes < photo) {
+    console.log("You can not print the photo");
 } else {
-    console.log("You can not print the photo!");
+    console.log("You can print the photo!");
 }
 
+// 
+var size = prompt("What is your photo resolution?");
+    console.log(size);
+
+
+// validation of the prompt
+
+if (size === ""){
+
+    size = prompt("Enter your photo resolution.");
+
+
+}
+
+if(size < minRes){
+    console.log("You can not print the photo.");
+}else{
+    console.log("You can print the photo.");
+
+
+}
+
+console.log("This " + minRes < size  + " resolution is large enough to print");
+alert("This " + minRes < size  + " resolution is large enough to print");

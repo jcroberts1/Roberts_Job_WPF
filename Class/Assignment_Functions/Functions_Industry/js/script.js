cef77da67ae449a81293f556792aa85217aa1a52
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-24 18:23:19 -0500

    Functions_personal

--- Class/Assignment_Functions/Functions_Industry/js/script.js
+++ Class/Assignment_Functions/Functions_Industry/js/script.js
@@ -1,14 +1,26 @@
-
-
-
-
-
-
+/*
+ //JavaScript
+ // Name: Job Roberts
+ //WPF 1410 Section 01
+ // Date: 23 Oct 2014
+ //Assignment:Functions.
+ */
 var name =prompt("Please enter your name.");
 
 while(name===""){
 //Validate the users input information.
-    name =prompt("Please enter your nam.");
+    name =prompt("Please enter your name.");
+    console.log(name);
+}
+
+function outptMsg() {
+    console.log("Greetings "+name+" I am a computer.");
 
 }
+
+
+//Invoking the function:
+outptMsg();
+
+
 

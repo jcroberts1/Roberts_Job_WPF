957d86cbffa4bc1ad820e3e7d432862a88e4b499
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-24 19:30:31 -0500

    Function created
    functions created and invoking created

--- Class/Assignment_Functions/Functions_Personal/js/script.js
+++ Class/Assignment_Functions/Functions_Personal/js/script.js
@@ -2,7 +2,28 @@
  //JavaScript
  // Name: Job Roberts
  //WPF 1410 Section 01
- // Date: 23 Oct 2014
- //Assignment:Functions.
+ // Date: 24 Oct 2014
+ //Assignment:Functions_Personal.
  */
+function makeAmazing(a, b){
+    //Code here to run
+    alert(a + b);
+//invoking the code
+}makeAmazing('Your computer is so smart' , ' and coding is fun.');
 
+var name =prompt("Enter your name");
+    console.log(name);
+
+if (name === ""){
+    //validate the user information
+    name = prompt("You forgot to enter your name");
+}
+//alert the user of info
+alert= prompt("Thank you for learning code "+ name +"");
+
+var an1 = ["you are welcome", "thank you"];
+
+
+if (an1)
+
+   document.write("Look at this");

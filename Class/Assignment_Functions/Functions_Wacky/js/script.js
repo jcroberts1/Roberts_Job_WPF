f8bbcf8624ed333a9b5acf39e7b999aadc7557b4
jcroberts1 <jcroberts1@fullsail.edu>
2014-10-24 21:34:13 -0500

    Image src added
    html edited with an image pop up

--- Class/Assignment_Functions/Functions_Wacky/js/script.js
+++ Class/Assignment_Functions/Functions_Wacky/js/script.js
@@ -2,8 +2,8 @@
  //JavaScript
  // Name: Job Roberts
  //WPF 1410 Section 01
- // Date: 23 Oct 2014
- //Assignment:Functions.
+ // Date: 24 Oct 2014
+ //Assignment:Functions Wacky.
  */
 var name =prompt("Please enter your name");
   console.log(name);
@@ -11,11 +11,23 @@ var name =prompt("Please enter your name");
 while(name===""){
     name = prompt("You forgot your name again");
 }
+document.write("Look at all those wacky dogs");
+console.log("Look at all those wacky dogs");
 
-function greet(name){
+function greet(name) {
     //code to run
 
-   alert("Hello " + name + "");
+    alert("Hello " + name + "");
+}
+alert =prompt("Hi "+name+" please enter an number");
+console.log(alert);
+
+//for loop 
+for (var i =20 ; i > 0; i--){
+    console.log(i + 'dogs in the yard')
+
 
 }
 greet(name);
+
+
